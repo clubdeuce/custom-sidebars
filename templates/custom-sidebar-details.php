9e215d3f2f3930ac7e1d29b73607bd00faac5873
Daryl Lozupone <daryl@clubduece.com>
2015-05-01 15:55:02 -0400

    Add missing "selected" option for "none" value in custom-sidebar-select

--- templates/custom-sidebar-details.php
+++ templates/custom-sidebar-details.php
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 <label for="custom-sidebar-select"><?php _e( 'Use the following sidebar for this page:', 'custom-sidebars' ); ?></label>
 <select name="custom-sidebar-select">
 	<option value=""><?php _e( 'Default Sidebar', 'custom-sidebars' ); ?></option>
-	<option value="none"><?php /* translators: as in 'No sidebar' */ _e( 'None', 'custom-sidebars' ); ?></option>
+	<option value="none" <?php selected( Custom_Sidebars::get_sidebar(), 'none'); ?>><?php /* translators: as in 'No sidebar' */ _e( 'None', 'custom-sidebars' ); ?></option>
 	<?php $metabox->sidebar_select_options(); ?>
 </select>
 
